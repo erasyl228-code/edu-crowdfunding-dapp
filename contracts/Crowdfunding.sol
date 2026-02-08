3e01d27433fc9b5c7b83b2d04c772f612bc4ef86
Erasyl <nemoerasylomirbay@gmail.com>
2026-02-09 00:45:43 +0500

    Add contribute function to Crowdfunding contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -14,4 +14,42 @@ contract Crowdfunding {
     Campaign[] public campaigns;
 
     mapping(uint256 => mapping(address => uint256)) public contributions;
+
+    // Функция для создания новой краудфандинговой кампании
+function createCampaign(
+    string memory _title,
+    uint256 _goal,
+    uint256 _durationInDays
+) public {
+    // создаём новый контракт кампании
+    Campaign memory newCampaign;
+    newCampaign.title = _title;
+    newCampaign.creator = payable(msg.sender);
+    newCampaign.goal = _goal;
+    newCampaign.collectedAmount = 0;
+    newCampaign.deadline = block.timestamp + (_durationInDays * 1 days);
+    newCampaign.finalized = false;
+
+    // добавляем кампанию в массив
+    campaigns.push(newCampaign);
+}
+
+// Функция для внесения средств в кампанию
+function contribute(uint256 _campaignId) public payable {
+    Campaign storage campaign = campaigns[_campaignId];
+
+    // проверка: кампания ещё не завершена
+    require(block.timestamp <= campaign.deadline, "Campaign has ended");
+
+    // проверка: пользователь отправил больше 0 ETH
+    require(msg.value > 0, "You need to send some ETH");
+
+    // обновляем вклад пользователя
+    contributions[_campaignId][msg.sender] += msg.value;
+
+    // обновляем общую собранную сумму
+    campaign.collectedAmount += msg.value;
+}
+
+
 }
